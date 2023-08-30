05e84cecc79b937efe81a63f9f617948c685f064
KislyjKisel <danilakisel@gmail.com>
2023-08-30 14:51:33 +0300

    Update toolchain to `v4.0.0-rc2`

--- lakefile.lean
+++ lakefile.lean
@@ -13,17 +13,17 @@ def ffiSrc := "native.c"
 def ffiO   := "ffi.o"
 def ffiLib := "ffi"
 
-target ffi.o (pkg : Package) : FilePath := do
+target ffi.o pkg : FilePath := do
   let oFile := pkg.buildDir / ffiO
   let srcJob ← inputFile <| pkg.dir / cDir / ffiSrc
   buildFileAfterDep oFile srcJob fun srcFile => do
     let flags := #["-I", (← getLeanIncludeDir).toString, "-fPIC"]
     compileO ffiSrc oFile srcFile flags
 
-extern_lib ffi (pkg : Package) := do
+extern_lib ffi pkg := do
   let name := nameToStaticLib ffiLib
   let ffiO ← fetch <| pkg.target ``ffi.o
-  buildStaticLib (pkg.buildDir / "lib" / name) #[ffiO]
+  buildStaticLib (pkg.nativeLibDir / name) #[ffiO]
 
 script examples do
   let examplesDir ← ("examples" : FilePath).readDir
